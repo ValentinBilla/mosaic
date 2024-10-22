0656959bffb7869a1d2aa0a5d7929ecd488adb74
Emma Gonin <emmagonin1@gmail.com>
2024-10-22 15:51:06 +0200

    250 ocean images bdd

--- scraper_bdd.py
+++ scraper_bdd.py
@@ -7,10 +7,12 @@ import os
 def scrape_image_urls(url):
     api_key = os.getenv('PEXELS_API_KEY')
     params = {
-        'query': 'flowers',
-        'orientation': 'landscape',
-        'per_page': 20,
-        'page': 1
+        # 'query': 'flowers',
+        'query': 'Ocean',
+        # 'color': '#3f53eb',
+        # 'orientation': 'landscape',
+        'per_page': 10,
+        'page': 7
     }
 
     headers = {
@@ -23,24 +25,31 @@ def scrape_image_urls(url):
         data = response.json()
         # images = BeautifulSoup(response.content, 'html.parser').find_all("photos")
         images=data["photos"]
+        theme=params["query"]
         if images :
-            image_urls = [image["src"]["original"] for image in images]
-            return image_urls
+            images_data=[]
+            for image in images:
+                image_url = image["src"]["original"]
+                color = image["avg_color"]
+                images_data.append((image_url, color, theme))
+
+            print(images_data)
+            return images_data
         
     else:
         print(f"Erreur : {response.status_code}")
 
-def save_to_database(image_urls):
+def save_to_database(images_data):
     db_path = 'images.db'
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     
-    cursor.execute('''CREATE TABLE IF NOT EXISTS image_urls
-                      (url TEXT PRIMARY KEY)''')
-    
-    for url in image_urls:
-        cursor.execute("INSERT OR IGNORE INTO image_urls (url) VALUES (?)", (url,))
-    
+    cursor.execute('''CREATE TABLE IF NOT EXISTS image_data
+                      (url TEXT PRIMARY KEY, avg_color TEXT, theme TEXT)''')
+
+    for url, color, theme in images_data:
+        cursor.execute("INSERT OR IGNORE INTO image_data (url, avg_color, theme) VALUES (?, ?, ?)", (url, color, theme))
+
     conn.commit()
     conn.close()
 
